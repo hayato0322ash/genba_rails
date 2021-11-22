8f57b4390895aa1b61472ab331e4734850da2fe3
hayato <hayato.drsp@gmail.com>
2021-11-22 18:01:01 +0900

    End Chapter 3

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'gemva-タスク管理ツール-'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
