a308b821f4dd47a1eeba491241f82d835f3de4f5
hayato <hayato.drsp@gmail.com>
2021-11-26 13:05:27 +0900

    End Chapter 6

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  root to: 'tasks#index'
+  resources :tasks
+
   namespace :admin do
     resources :users
   end
 
-  root to: 'tasks#index'
-  resources :tasks
+  controller :sessions do
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
 end
